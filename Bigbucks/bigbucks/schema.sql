b4e8867a83f2d0ef4a1df0f6fdf34f0d25034302
leyao.jia@duke.edu <lj218@duke.edu>
2024-03-30 16:06:49 -0400

    Replace schema.sql

---
+++ Bigbucks/bigbucks/schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS HistoricalStockData;
+
+CREATE TABLE user (
+    UserId INTEGER PRIMARY KEY AUTOINCREMENT,
+    UserName TEXT UNIQUE NOT NULL,
+    Password TEXT NOT NULL
+    AccountBalance DECIMAL NOT NULL
+);
+
+CREATE TABLE stocks (
+    StockID INTEGER PRIMARY KEY AUTOINCREMENT,
+    TickerSymbol TEXT NOT NULL UNIQUE,
+    CompanyName TEXT NOT NULL
+);
+
+CREATE TABLE transactions (
+    TransactionID INTEGER PRIMARY KEY AUTOINCREMENT,
+    UserID INTEGER NOT NULL,
+    StockID INTEGER NOT NULL,
+    TransactionType TEXT NOT NULL CHECK(TransactionType IN ('BUY', 'SELL')),
+    Quantity INTEGER NOT NULL,
+    PricePerShare REAL NOT NULL,
+    TransactionDate DATETIME NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (StockID) REFERENCES Stocks(StockID)
+);
+
+CREATE TABLE HistoricalStockData (
+    StockID INTEGER NOT NULL,
+    Date DATE NOT NULL,
+    OpenPrice REAL NOT NULL,
+    ClosePrice REAL NOT NULL,
+    HighPrice REAL NOT NULL,
+    LowPrice REAL NOT NULL,
+    Volume INTEGER NOT NULL,
+    PRIMARY KEY (StockID, Date),
+    FOREIGN KEY (StockID) REFERENCES Stocks(StockID)
+);
